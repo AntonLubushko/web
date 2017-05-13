ca313c29b26c4b979b44e2373e8d4c78b1200834
Ilya <ilya.lysenko@moonion.com>
2017-05-13 19:10:21 +0300

    must finish return in UsersList

--- app/layout/users/UsersList.jsx
+++ app/layout/users/UsersList.jsx
@@ -12,28 +12,61 @@ Parse.serverURL = 'http://localhost/parse';
 // Fetching all users with all fields and print to console
 let Users = Parse.Object.extend("Users");
 let query = new Parse.Query(Users).include(['Town']);
-query.find()
-.then(resolve => {
-  resolve.forEach(user => {
-	 		let name = user.get("name");
-				console.log("Id ", user.id);
-				console.log("Name ", name);
-				console.log("Email ", user.get('email'));
-				console.log("Town ", user.get('Town').get('name'));
+// query.find()
+// .then(resolve => {
+//   resolve.forEach(user => {
+// 	 		let name = user.get("name");
+// 				console.log("Id ", user.id);
+// 				console.log("Name ", name);
+// 				console.log("Email ", user.get('email'));
+// 				console.log("Town ", user.get('Town').get('name'));
 			
-		});
+// 		});
 			
-})
-.catch(err=>{
-    console.log('ERR::::', err);
-})
+// })
+// .catch(err=>{
+//     console.log('ERR::::', err);
+// })
 
 export default class UsersList extends Component {
-    render() {
-        return (
-            <h1 style={styles.indexPage} >
-                Users
-            </h1>
-        );
-    }
+
+constructor(props){
+	super(props);
+	this.state = {
+		array: [],
+	}
+}
+
+componentDidMount(){
+		
+		return query.find()
+		.then(resolve => {
+			let array = resolve;
+			this.setState({array});
+})
+}
+
+
+	render() {
+let all = [];
+for(let i = 0; i < this.state.array.length;  i++){
+
+	let name =  this.state.array[i]? this.state.array[i].get("name") : '';
+	let email =  this.state.array[i]? this.state.array[i].get("email") : '';
+	let town =  this.state.array[i]? this.state.array[i].get("Town").get("name") : '';
+	all.push([name, email, town]);
+}
+console.log(all);
+
+
+// this return must be finished
+return (
+		<div>
+		{all.map( (user, i) => <p key = {i}>{user.name}</p>)}
+		
+		</div>
+	);
+
+
+}
 };
